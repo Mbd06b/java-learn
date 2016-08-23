6dd782bb2e846fdd5746d9ca61d18034d526f565
Matthew Dowell <mbd06b@gmail.com>
2016-08-22 21:30:23 -0500

    renaming

---
+++ 01-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+//============================================================================
+// DESCRIPTION : HelloWorld.java
+// CLASS       : COSC 1337.S70
+// AUTHOR      : Matthew Dowell
+// DATE        : 08/22/2015
+//============================================================================
+
+
+public class HelloWorld
+{
+		public static void main(String[] args) {
+			System.out.println ( "Hello World, from Matthew!!!" );
+		}
+}
