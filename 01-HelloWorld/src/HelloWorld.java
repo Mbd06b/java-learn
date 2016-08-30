a070f696dc34c71eeb46ad55b5f83de2653207fe
Matthew Dowell <mbd06b@gmail.com>
2016-08-29 21:37:08 -0500

    Playing around with hello world.

--- 01-HelloWorld/src/HelloWorld.java
+++ 01-HelloWorld/src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.io.InputStream;
+import com.sun.java_cup.internal.runtime.Scanner;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Import;
+
 //============================================================================
 // DESCRIPTION : HelloWorld.java
 // CLASS       : COSC 1337.S70
@@ -10,5 +14,32 @@ public class HelloWorld
 {
 		public static void main(String[] args) {
 			System.out.println ( "Hello World, from Matthew!!!" );
+			
+			int answer; 
+			String firstString = "Hello World, from Matthew's String";
+			
+			
+			answer = 2 + 2; 
+			
+			System.out.println (answer);
+			System.out.println ("Java is not a drink");
+			System.out.println ("You like tea");
+			
+			System.out.println (firstString + " the answer is " + answer); 
+			
+			firstString = "I don't think you can do this";
+			
+			System.out.println (firstString + ". Well I was wrong, you can");
+			
+			boolean test = false; 
+			
+			if(firstString == "I don't think you can do this"){
+				test = true; 
+			}else{
+				test = false; 
+			}
+			System.out.println(test);
+			
+			
 		}
 }
