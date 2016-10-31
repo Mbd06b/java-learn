0a5f86c981d37bffcc6c0486f5f13759a42ab748
mbd06b <mbd06b@mbd06b-ThinkPad-E555>
2016-10-31 12:28:49 -0500

    Lab 5 Hot Dog Stand Class Complete

--- HotDogStand/HotDogStand.java
+++ HotDogStand/HotDogStand.java
@@ -1,7 +1,16 @@
 import java.lang.reflect.Array;
+import java.util.Objects;
+import java.util.Scanner;
+
+//============================================================================
+//DESCRIPTION : Lab 5 - Hot Dog Stand Class 
+//CLASS       : COSC 1337.S70  (INEW 2230.S01)
+//AUTHOR      : Matthew Dowell
+//DATE        : 10/31/2015
+//============================================================================
 
 /*
- * pg. 339 Proj. 1
+ * Savich Absolute Java pg. 339 Proj. 1
  * You operate several hot dog stands distributed throughout town. Define a class named HotDogStand that has an instance
  * variable for the hot dog stand's ID number and an instance variable for how many hot dogs the stand has sold that day. 
  * Create a constructor that allows a user of the class to initialize both values. 
@@ -21,8 +30,10 @@ public class HotDogStand {
 	
 	private static 	int totalHotDogsSold = 0;
 	
-	private 	int [] dogsSold = new int [HotDog.values().length];//returns the #of items in the enum, -1 for base 0 array.  
+	private 	 int [] dogsSold = new int [HotDog.values().length];//returns the #of items in the enum. 
 	private    	 String idNumber;
+	private		boolean disposeNotCalled; 
+	private 	 	int timesCopied = 1; //THE ORIGINAL
 	
 	
 	
@@ -36,23 +47,24 @@ public class HotDogStand {
 			TOFU   (4, "Vegan Bun with tofu dog, 100% fillers!"),
 			KOSHER (5, "Standard Hebrew National bun and dog.");
 		
-		private String description;
+		private	   String description;
 		private final int iNum; //to create a number value to better index elsewhere.  
+		
 		private HotDog(int iNum, String description){
-			this.description = description;
-			this.iNum = iNum; 
-		};
+					   this.description = description;
+					   this.iNum = iNum; 
+				};
 		
 
-		public int geti(){return iNum;}; 
+		public 	  int 	 geti(){return iNum;}; 
 		public String getDesc(){return description;}
 		
 		//create enum Strings in a preferred print format CLASSIC becomes Classic; 
 		@Override
 		public String toString(){
-			String constName = super.toString();
-			return constName.substring(0, 1) + constName.substring(1).toLowerCase(); 
-		}
+						String constName = super.toString();
+						return constName.substring(0, 1) + constName.substring(1).toLowerCase(); 
+					  }
 	}
 
 	
@@ -63,6 +75,7 @@ public class HotDogStand {
 			dogsSold[i] = 0; 
 		}
 		idNumber = null; 
+		disposeNotCalled = true; 
 	}
 	
 //Other Constructor
@@ -70,82 +83,63 @@ public class HotDogStand {
 		for(int i = 0; i < (HotDog.values().length); i++){
 			dogsSold[i] = 0; 
 		}
-		this.idNumber = idNumber; 
+		this.idNumber = idNumber;
+		disposeNotCalled = true;
 	}
+	
 	
 //Copy Constructor
 	HotDogStand (HotDogStand toCopy){
-		this.dogSold[] = toCopy.dogsSold[];
-		this.idNumber = toCopy.idNumber; 
+		if(toCopy == null){
+			 throw new NullPointerException("Null argument - Copy Constructor" ); 
+		 }
+		   toCopy.timesCopied++; //increases original 1, to 2 and so on. 
+		   this.idNumber = toCopy.idNumber;
+		   this.idNumber = toCopy.idNumber + " #" + toCopy.timesCopied; 
+	 //In a practical sense, I don't think the copy constructor should copy these Hot Dogs Sold count values. 
+	 //but for the theoretical purpose of creating an exact copy.. here's what that looks like: 
+		this.dogsSold = toCopy.dogsSold;
+		totalHotDogsSold += toCopy.getStandTotal(); //increases our static counter.   
 	}
 
 	
 //Methods
-	
-	public static int getTotalDogsSold(){return totalHotDogsSold;}
+	public static int getTotalDogsSold()			 {return totalHotDogsSold;}
+	public		  int getDogOfTypeSold(HotDog option){return dogsSold[option.geti()];}
+	public	   String	   getIdNumber()		     {return idNumber;}
+	public		  int   getTimesCopied()			 {return timesCopied  - 1;} //- 1 so the original shows as 0; 
 	public 		  int    getStandTotal(){
-						int sum = 0; 
+						 	int sum = 0; 
 							for(int i = 0; i < (HotDog.values().length); i++){
 								sum += dogsSold[i]; 
 							}
 							return sum; 
+						 }
+	
+	public 	  boolean      setidNumber(String str){
+										if(this.equals(str)){
+											return false;
+										}else{
+										this.idNumber = str; return true;}
+							}
+		
+	public 	  boolean	   setDogsSold(HotDog option, int quantity){
+	    if ( (getDogOfTypeSold(option)+ quantity) < 0){
+	        throw new IllegalArgumentException("Hot Dogs Sold cannot be negative.");
+	    }else{
+			if(quantity < 0){
+				Scanner keyboard = new Scanner (System.in); //System.in is the console.
+		    	System.out.println("By using a negative value you are canceling a sale, are you sure you want to continue? (Enter Y or N)\n");
+				String confirm = keyboard.next(); 
+						if(!confirm.equals("Y")){
+							System.out.println("Transaction Cancelled");
+							keyboard.close();
+							return false; 
 						}
-	
-	boolean 	shutDown
-
-	
-	public String toString (){
-	
-		String tempString = this.idNumber + " has sold: \n";
-		int i = 0;
-		for (HotDog d : HotDog.values()) {
-		     tempString += d + ":[" + Array.getInt(dogsSold, i) + "] ";
-		     if(i < (HotDog.values().length)){
-		    	 i++;
-		     }else;
+				keyboard.close(); 
 		 }
-		tempString += "\nStand Total Sold:" + getStandTotal(); 
-		return tempString; 
-}
-			
-	public boolean sellDog (HotDog option){
-		
+		    	
 			switch(option){
-				case CLASSIC: dogsSold[option.geti()]++; 
-							  System.out.println("Sold 1 " + option);
-							  break;			  
-				case CHICAGO: dogsSold[option.geti()]++; 
-							  System.out.println("Sold 1 " + option);
-							  break;				  
-				case NEWYORK: dogsSold[option.geti()]++; 
-							  System.out.println("Sold 1 " + option);
-				  			  break;
-				case  GERMAN: dogsSold[option.geti()]++; 
-							  System.out.println("Sold 1 " + option);
-				  			  break;
-				case    TOFU: dogsSold[option.geti()]++; 
-							  System.out.println("Sold 1 " + option);
-				  			  break;  
-				case  KOSHER: dogsSold[option.geti()]++; 
-							  System.out.println("Sold 1 " + option);
-				  			  break;  
-				default: System.out.println("Invalid Hotdog Selection");
-						return false; 
-			}
-			totalHotDogsSold++; 
-			return true; //dogsSold was incremented.  
-	}; 
-	
-	
-	
-	public boolean  justSold(){
-						sellDog(HotDog.CLASSIC);
-						return true; 
-					}
-	
-	public boolean sellDogs (HotDog option, int quantity){
-		
-		switch(option){
 			case CLASSIC: dogsSold[option.geti()] =  dogsSold[option.geti()] + quantity;
 						  System.out.println("Sold " + quantity + " " + option);
 						  break;			  
@@ -167,12 +161,62 @@ public class HotDogStand {
 			default: System.out.println("Invalid Hotdog Selection");
 					return false; 
 		}
-		totalHotDogsSold += quantity; 
-		return true; //dogsSold was incremented.  
+	totalHotDogsSold += quantity; 
+	return true; //dogsSold was incremented.  
+				
+	    }			
+		    
+}
+	public boolean  justSold(){
+		setDogsSold(HotDog.CLASSIC, 1);
+		return true; 
+	}
+	
+	
+	public boolean sellDog (HotDog option){
+				  setDogsSold(option, 1); 
+				  return true;
 	}; 
 
+	public	String toString (){
+		
+		String tempString = this.idNumber + " has sold: \n";
+		int i = 0;
+		for (HotDog d : HotDog.values()) {
+		     tempString += d + ":[" + Array.getInt(dogsSold, i) + "] ";
+		     if(i < (HotDog.values().length)){
+		    	 i++;
+		     }else;
+		 }
+		tempString += "\nStand Total Sold:" + getStandTotal(); 
+		return tempString; 
+}
 	
-
+	
+	public void finalize (){
+		
+		if(disposeNotCalled){
+		System.out.println("Person - finalize method");
+		}
+	}
+	
+	public void dispose (){
+		disposeNotCalled = false; 
+		System.out.println("Person - dispose method"); 
+	}
+	
+	public boolean equals (Object stand){
+		if(this == stand){
+			return true;
+		}
+		if(stand == null || getClass() != stand.getClass()){
+			return false; 
+		}
+		
+		HotDogStand stand1 = (HotDogStand)stand; 
+		return Objects.equals(idNumber, stand1.idNumber)
+				&& Objects.equals(dogsSold, stand1.dogsSold);
+	}
 	
 	
 }
--- HotDogStand/HotDogStandTest.java
+++ HotDogStand/HotDogStandTest.java
@@ -1,18 +1,27 @@
 
+
+//============================================================================
+// DESCRIPTION : Lab 5 - Hot Dog Stand Class 
+// CLASS       : COSC 1337.S70  (INEW 2230.S01)
+// AUTHOR      : Matthew Dowell
+// DATE        : 10/31/2015
+//============================================================================
+
+
 public class HotDogStandTest{
 	
 
 	public static void main (String [] args){
 		
-		HotDogStand.HotDog menu = null; 
+
 		HotDogStand firstStand = new HotDogStand("Matthew's Hot Dogs");
 		
 		//System.out.println(firstStand);
 		
-		firstStand.sellDogs(HotDogStand.HotDog.NEWYORK, 15 ); 
-		firstStand.justSold(); 
+		firstStand.setDogsSold(HotDogStand.HotDog.NEWYORK, 15 ); 
+		firstStand.justSold(); //sells 1 classic dog.  
 		firstStand.sellDog(HotDogStand.HotDog.GERMAN); 
-		firstStand.sellDogs(HotDogStand.HotDog.CLASSIC, 3);
+		firstStand.setDogsSold(HotDogStand.HotDog.CLASSIC, 3);
 		firstStand.sellDog(HotDogStand.HotDog.KOSHER); 
 		System.out.println(firstStand);
 		
@@ -21,20 +30,47 @@ public class HotDogStandTest{
 		
 		HotDogStand secondStand = new HotDogStand("Corner Park Brats"); 
 		
-		secondStand.sellDogs(HotDogStand.HotDog.TOFU, 2);
+		secondStand.setDogsSold(HotDogStand.HotDog.TOFU, 2);
 		secondStand.sellDog(HotDogStand.HotDog.CLASSIC);
-		secondStand.sellDogs(HotDogStand.HotDog.CHICAGO, 2);
-		secondStand.sellDogs(HotDogStand.HotDog.KOSHER, 10);
+		secondStand.setDogsSold(HotDogStand.HotDog.CHICAGO, 2);
+		secondStand.setDogsSold(HotDogStand.HotDog.KOSHER, 10);
 		System.out.println(secondStand);
 		System.out.println("Total Hot Dogs Sold: " + HotDogStand.getTotalDogsSold()+ "\n\n");
+		
+		
+		secondStand.setidNumber("Central Park Brats");
+		System.out.println("Renaming and manual accessor 'get' test, Corner Park Brats is now, " + secondStand.getIdNumber() + " Which has sold:");
+	    System.out.print("Classic:["   + secondStand.getDogOfTypeSold(HotDogStand.HotDog.TOFU) + "]" +
+	    				 " Chicago:["  + secondStand.getDogOfTypeSold(HotDogStand.HotDog.CHICAGO) +"]" +
+	    				 " New York:[" + secondStand.getDogOfTypeSold(HotDogStand.HotDog.NEWYORK) +"]" +
+	    				 " German:["   + secondStand.getDogOfTypeSold(HotDogStand.HotDog.GERMAN) +"]" +
+	    				 " Tofu:[" 	   + secondStand.getDogOfTypeSold(HotDogStand.HotDog.TOFU) +"]" +
+	    				 " Kosher:["   + secondStand.getDogOfTypeSold(HotDogStand.HotDog.KOSHER) + "]\n" +
+	    				 "Total Stand Sold: " + secondStand.getStandTotal() + "\nTotal Hot Dogs Sold: " + HotDogStand.getTotalDogsSold()+ "\n\n");
+	    
 		
 		
 		HotDogStand thirdStand = new HotDogStand("Grime's Special Weiners");
 		thirdStand.justSold(); 
 		thirdStand.justSold(); 
-		thirdStand.sellDogs(HotDogStand.HotDog.CHICAGO, 3);
-		thirdStand.sellDogs(HotDogStand.HotDog.TOFU, 10);
+		thirdStand.setDogsSold(HotDogStand.HotDog.CHICAGO, 3);
+		thirdStand.setDogsSold(HotDogStand.HotDog.TOFU, 10);
 		System.out.println(thirdStand);
+		System.out.println("Total Hot Dogs Sold: " + HotDogStand.getTotalDogsSold()+ "\n\n");
+		
+		System.out.println("Copy Constructor Test, copied Grime's Special Weiners along with it's sales data (see copy constructor comments)");
+		HotDogStand fourthStand = new HotDogStand(thirdStand); //Copy Constructor Test
+		System.out.println(fourthStand);
+		System.out.println("Total Hot Dogs Sold: " + HotDogStand.getTotalDogsSold()+ "\n\n");
+		
+		System.out.println("Refund Test - 10 New York HotDogs Returned to Matthew's Stand");
+		firstStand.setDogsSold(HotDogStand.HotDog.NEWYORK, -10);
+		System.out.println(firstStand);
+		System.out.println("Total Hot Dogs Sold: " + HotDogStand.getTotalDogsSold() + "\n\n");
+		
+		System.out.println("Refund Exception Test - 16 New York HotDogs Returned to Matthew's Stand");
+		firstStand.setDogsSold(HotDogStand.HotDog.NEWYORK, -16);
+		System.out.println(firstStand);
 		System.out.println("Total Hot Dogs Sold: " + HotDogStand.getTotalDogsSold() + "\n\n");
 		
 	}
